299aa49667c52cc354084e7bdae95c615dd84b10
coco <308695699@qq.com>
2017-09-18 21:59:39 +0800

    achieve sort function

--- js/popup.js
+++ js/popup.js
@@ -4,33 +4,58 @@ $(function () {
         var key = localStorage.getItem('key');
         console.log('key', key);
 
-
-        function createTpl(obj) {
+        /**
+         * 根据参数个数创建 DOM 结构
+         * @param {JSON String} obj 
+         * @param {Array} sortKeys 
+         */
+        function createTpl(obj, sortKeys) {
             var paramsObj = JSON.parse(obj);
             var dom = "";
+            var argLength = arguments.length;
 
-            for (var key in paramsObj) {
-                var value = paramsObj[key];
+            if(argLength === 1) {
+                for (var key in paramsObj) {
+                    var value = paramsObj[key];
 
-                dom += `<li>
-                            <div class="field is-horizontal">
-                                <div class="field-label ">
-                                    <label class="label">${key}</label>
-                                </div>
-                                <div class="field-body">
-                                    <div class="field">
-                                        <div class="control">
-                                            <input class="input is-small paramValue" type="text" placeholder="empty" value="${value}">
-                                        </div>
+                    dom += tpl(key, value);
+                }
+            } else if(argLength === 2) {
+                for (var j=0; j<sortKeys.length; j++) {
+                    var sortkey = sortKeys[j];
+
+                    dom += tpl(sortkey, paramsObj[sortkey]);
+                }
+            }
+
+            $('.g-list').html('').append(dom);
+        }
+
+        /**
+         * 单个参数 li 模板
+         * @param {String} key 
+         * @param {String} value 
+         */
+        function tpl(key, value) {
+            return `<li>
+                        <div class="field is-horizontal">
+                            <div class="field-label ">
+                                <label class="label">${key}</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <div class="control">
+                                        <input class="input is-small paramValue" type="text" placeholder="empty" value="${value}">
                                     </div>
                                 </div>
                             </div>
-                        </li>`;
-            }
-
-            $('.g-list').append(dom);
+                        </div>
+                    </li>`;
         }
 
+        /**
+         * 事件绑定
+         */
         function evnetBind() {
             $(document).on('click', '.j-decode', function(e) {
                 var params = $('.paramValue');
@@ -53,6 +78,30 @@ $(function () {
                     elem.val(encodeURIComponent(elem.val()));
                 }
             });
+
+            $(document).on('click', '.j-sort', function(e) {     
+                var sortKey = sort(key);
+
+                createTpl(key, sortKey);
+            });
+        }
+
+        /**
+         * 对参数排序
+         * @param {JSON String} obj 
+         */
+        function sort(obj) {
+            var paramsObj = JSON.parse(obj);
+
+            keysSorted = Object.keys(paramsObj).sort(function(a, b){
+                if (a[0].toLowerCase() > b[0].toLowerCase()) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+
+            return keysSorted;
         }
 
         return {
